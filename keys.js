91abf85194463b6eede2294f0f41b35a515dc8c6
Michelle Hettinger <Michelle Hettinger>
2016-08-23 19:26:18 -0400

    added keys and tokens for twitter

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
-console.log('this is loaded');
+console.log('Keys is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'LNGNfcPl7njoCLHdolRxcn9Xd',
+  consumer_secret: 'xDMmP2DlknLuLnKDXEKAampK8yLvBVkRYMFwSo5l9ihWeTOgyo',
+  access_token_key: '1556695459-02dxnkjDDDxFddLiXaYqgVAWgAukuq3TsRlBmrx',
+  access_token_secret: 'Yjlcv06R54XzaBthzeV5MhCO161kpwaVk3hyR2LCNxVic',
 }
